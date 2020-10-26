f83bd6794cf2c85f9b5ea0f2fee60b3c866b6d98
ahnteahyeon <ahnteahyeon@gmail.com>
2020-10-26 22:07:38 +0900

    styled-components를 이용한 GlobalStyles Component 생성

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+    * {
+        box-sizing: border-box;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    body {
+        font-size: 14px;
+        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+`;
+
+export default globalStyles;
